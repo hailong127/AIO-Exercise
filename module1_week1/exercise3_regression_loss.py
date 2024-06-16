819d0b8ef9ed6e58422c0efb6dd52bc84fe56e1f
hailong127 <hailong1272003@gmail.com>
2024-06-16 17:21:13 +0700

    Exercise-week1

---
+++ module1_week1/exercise3_regression_loss.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+def mae(y, y_hat):
+    return np.mean(np.abs(y - y_hat))
+
+
+def mse(y, y_hat):
+    return np.mean(np.square(y - y_hat))
+
+
+def rmse(y, y_hat):
+    return np.sqrt(mse(y, y_hat))
+
+
+def print_statement(loss_name, sample_num, y, y_hat, loss_val):
+    print(
+        f"loss name: {loss_name.upper()}, sample: {sample_num}, pred: {y}, target: {y_hat}, loss: {loss_val}")
+
+
+def main():
+    num_samples = input(
+        "Input number of samples (integer number) which are generated: ")
+    if not num_samples.isnumeric():
+        print("Number of samples must be an integer number.")
+        return
+
+    num_samples = int(num_samples)
+    loss_func = input("Input loss name (mae, mse, rmse): ")
+    loss_func = loss_func.lower()
+    if loss_func not in ["mae", "mse", "rmse"]:
+        print("Loss function must be one of the following: mae, mse, rmse.")
+        return
+
+    rnd_gen = np.random.default_rng(seed=42)
+    for i in range(num_samples):
+        y = rnd_gen.uniform(0, 10)
+        y_hat = rnd_gen.uniform(0, 10)
+        if loss_func == "mae":
+            loss_val = mae(y, y_hat)
+        elif loss_func == "mse":
+            loss_val = mse(y, y_hat)
+        else:
+            loss_val = rmse(y, y_hat)
+
+        print_statement(loss_func, i, y, y_hat, loss_val)
+
+
+if __name__ == "__main__":
+    main()
---
+++ module1_week1/trac_nghiem.py
@@ -0,0 +1,168 @@
+# câu hỏi 1:
+import math
+
+
+def calc_f1_score(tp, fp, fn):
+    precision = tp/(tp+fp)
+    recall = tp/(tp+fn)
+    f1_score = 2*(precision*recall)/(precision+recall)
+    return f1_score
+
+
+assert round(calc_f1_score(tp=2, fp=3, fn=5), 2) == 0.33
+print(round(calc_f1_score(tp=2, fp=4, fn=5), 2))
+
+# câu hỏi 2:
+
+
+def is_number(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    return True
+
+
+assert is_number(3) == 1.0
+assert is_number('-2a') == 0.0
+print(is_number(1))
+print(is_number('n'))
+
+# câu hỏi 3: ReLU
+
+# câu hỏi 4:
+
+
+def calc_sig(x):
+    sigmoid = 1/(1+math.exp(-x))
+    return sigmoid
+
+
+assert round(calc_sig(3), 2) == 0.95
+print(round(calc_sig(2), 2))
+
+# câu hỏi 5
+
+
+def calc_elu(x):
+    if x <= 0:
+        return 0.01*(math.exp(x)-1)
+    else:
+        return x
+
+
+assert round(calc_elu(1)) == 1
+print(round(calc_elu(-1), 2))
+
+# câu hỏi 6
+
+
+def calc_activation_func(x, act_name):
+    if act_name == 'elu':
+        return calc_elu(x)
+    elif act_name == 'sigmoid':
+        return calc_sig(x)
+    elif act_name == 'relu':
+        if x <= 0:
+            return 0
+        elif x > 0:
+            return x
+    else:
+        return ('sai tên activation function')
+
+
+assert calc_activation_func(x=1, act_name='relu') == 1
+print(round(calc_activation_func(x=3, act_name='sigmoid'), 2))
+
+# câu hỏi 7
+
+
+def calc_ae(y, y_hat):
+    return abs(y-y_hat)
+
+
+y = 1
+y_hat = 6
+assert calc_ae(y, y_hat) == 5
+y = 2
+y_hat = 9
+print(calc_ae(y, y_hat))
+
+# câu hỏi 8
+
+
+def calc_se(y, y_hat):
+    return (y-y_hat)**2
+
+
+y = 4
+y_hat = 2
+assert calc_se(y, y_hat) == 4
+print(calc_se(2, 1))
+
+# câu hỏi 9
+
+
+def giai_thua(n):
+    a = 1
+    for i in range(1, n+1):
+        a = a*i
+    return a
+
+
+def approx_cos(x, n):
+    sum = 0
+    for i in range(n+1):
+        sign = (-1)**i
+        term = sign*(x**(2*i)/giai_thua(2*i))
+        sum = sum + term
+    return sum
+
+
+assert round(approx_cos(x=1, n=10), 2) == 0.54
+print(round(approx_cos(x=3.14, n=10), 2))
+
+# câu hỏi 10
+
+
+def approx_sin(x, n):
+    sum = 0
+    for i in range(n+1):
+        sign = (-1)**i
+        term = sign*(x**(2*i+1)/giai_thua(2*i+1))
+        sum = sum + term
+    return sum
+
+
+assert round(approx_sin(x=1, n=10), 4) == 0.8415
+print(round(approx_sin(x=3.14, n=10), 4))
+
+# câu hỏi 11
+
+
+def approx_sinh(x, n):
+    sum = 0
+    for i in range(n+1):
+        term = (x**(2*i+1)/giai_thua(2*i+1))
+        sum = sum + term
+    return sum
+
+
+assert round(approx_sinh(x=1, n=10), 2) == 1.18
+print(round(approx_sinh(x=3.14, n=10), 2))
+
+# câu hỏi 12
+
+
+def approx_cosh(x, n):
+    sum = 0
+    for i in range(n+1):
+        term = (x**(2*i)/giai_thua(2*i))
+        sum = sum + term
+    return sum
+
+
+assert round(approx_cosh(x=1, n=10), 2) == 1.54
+print(round(approx_cosh(x=3.14, n=10), 2))
+
+# câu hỏi 13: A
